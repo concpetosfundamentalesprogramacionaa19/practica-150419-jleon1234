42928010db907b3308e9c89eb15ab3589355d6fe
Juan Pablo <jleon4811@gmail.com>
2019-04-15 16:43:54 -0500

    Subiendo presentacion dos

---
+++ presentacion2.py
@@ -0,0 +1,11 @@
+"""
+    Este es mi tercer ejemplo
+    en python
+
+    Ingreso de infromacion por teclado
+    Usuario Git: jleon1234
+"""
+nombre = input ('Porfavor ingrese su nombre: \n')
+apellido = input ("Por Favor ingrese su apellido: \n")
+print("Mi nombre es: %s \n\t mi apellido es:\n%s" % (nombre, apellido))
+# print(nombre, apellido)
